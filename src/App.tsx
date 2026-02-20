466b0815df3e53bbaa583066c3aad341eca9daa2
alifoo <alissonayres.trb@gmail.com>
2026-02-19 23:40:18 -0300

    chore: add tailwind:

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import { greet } from '../pkg/building_club'
 function App() {
   return (
     <>
-      <h1>Vite + React + WebAssembly</h1>
-      {greet("React CWB")}
+      <div className="flex border-2">
+        <h1>Vite + React + WebAssembly</h1>
+        {greet("React CWB")}
+      </div>
     </>
   )
 }
