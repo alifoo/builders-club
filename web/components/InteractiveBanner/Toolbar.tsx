25faaceeaff71ae7ad4bea8ef3404ea0a0e6a263
alifoo <alissonayres.trb@gmail.com>
2026-02-22 22:42:39 -0300

    feat: tech icons in toolbar

--- web/components/InteractiveBanner/Toolbar.tsx
+++ web/components/InteractiveBanner/Toolbar.tsx
@@ -1,5 +1,6 @@
 import { motion } from "motion/react";
 import { useState } from "react";
+import { SiWebassembly, SiJavascript } from "react-icons/si";
 
 interface ToolbarProps {
   xPercent: number;
@@ -21,8 +22,8 @@ const Toolbar = ({ xPercent, yPx, width, onMouseDown }: ToolbarProps) => {
         animate={{ opacity: 1, scale: 1 }}
         className="flex flex-col bg-white border-1 border-gray-200 p-1 rounded-xl text-[10px] font-space-mono"
       >
-        <button className={`${activeIndex === 0 ? "transition delay-150 duration-300 ease-in-out hover:-translate-y-[2px] hover:scale-103 hover:bg-gray-100" : ""} rounded-lg p-2`} onMouseOver={() => setActiveIndex(0)}>Color</button>
-        <button className={`${activeIndex === 1 ? "transition delay-150 duration-300 ease-in-out hover:-translate-y-[2px] hover:scale-103 hover:bg-gray-100" : ""} rounded-lg p-2`} onMouseOver={() => setActiveIndex(1)}>Grayscale</button>
+        <button className={`${activeIndex === 0 ? "transition delay-150 duration-200 ease-in-out hover:-translate-y-[2px] hover:scale-103 hover:bg-gray-100" : ""} rounded-lg p-2`} onMouseOver={() => setActiveIndex(0)}>Grayscale <SiWebassembly className="inline text-indigo-600" /></button>
+        <button className={`${activeIndex === 1 ? "transition delay-150 duration-200 ease-in-out hover:-translate-y-[2px] hover:scale-103 hover:bg-gray-100" : ""} rounded-lg p-2`} onMouseOver={() => setActiveIndex(1)}>Grayscale <SiJavascript className="inline text-yellow-400" /></button>
       </motion.div>
     </div>
   );
